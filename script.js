d076fb9493ec0ef79469c8b20c8aedc05ee2392b
Siddhesh Ghodke <siddheshghodke27@gmail.com>
2026-02-06 19:08:05 +0530

    Update script.js
    For Mobile Menu Toggale

--- script.js
+++ script.js
@@ -92,4 +92,19 @@ form.addEventListener('submit', function(e) {
             successMessage.classList.add('hidden');
         }, 5000);
     }
+
+});
+
+// Mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+document.querySelectorAll('#nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('show');
+    });
 });
